7286546acd55a668d2ba5b4e90aa6a39d6be3028
nicholasjamessmith <njsmith6@gmail.com>
2023-05-03 23:35:35 -0400

    fizz buzz attempted

--- answers.js
+++ answers.js
@@ -31,6 +31,26 @@ for (let i = 0; i <= 200; i += 2) {
 // Fizz Buzz
 ////////////////////////////////
 //Create a javascript application that logs all the numbers from 1 through 100. If a number is divisible by 3 log "Fizz" instead of the number. If a number is divisible by 5 log "Buzz" instead of the number. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+//Create a for loop that logs the numbers from 1 - 100. Create a function that prints "Fizz" instead of numbers divisible by 3. Create a function that prints "Buzz" instead of numbers divisible by 5. Create a function that prints "Fizz Buzz" instead of numbers divisible by both three and five. Nest the function within the for loop.
+
+//for let (i = 1; i <= 100; i++) {
+//  if (i / 3 % 0) {
+//    console.log("Fizz")
+//  }
+//  if (i % 3 == 0) {
+//    console.log("Buzz")
+//  }
+//  if (i % 3 == 0); (i % 5 == 0) {
+//    console.log("FizzBuzz")
+//  }
+//}
+//console.log(i)
+
+
+//let number = x
+//if x / 3 % 0 console.log("Fizz")
+//if x / 5 % 0 console.log("Buzz")
+//if x / 3 % 0 && x / 5 % 0 console.log("FizzBuzz")
 
 ////////////////////////////////
 // Wild Wild Life
