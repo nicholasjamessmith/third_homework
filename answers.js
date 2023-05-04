69756a334bc541519685f31ae4fe553a83e700b8
nicholasjamessmith <njsmith6@gmail.com>
2023-05-03 23:05:58 -0400

    part 1, easy going, get even answered

---
+++ answers.js
@@ -0,0 +1,62 @@
+///////////////PART 1///////////
+//HTML & CSS///
+//HTML Intro - 3
+//HTML Elements - 3
+//CSS Intro - 4
+//CSS Basics - 4
+
+
+//////////////PART 2////////////
+// Easy Going
+////////////////////////////////
+//Create a for loop that prints the numbers 1 through 20 when logged. Create a number variable equal to 1; make the variable less than or euqal to 20; write a funtion that continuously adds the variables within the set parameters.
+for (let i = 1; i <= 20; i++) {
+  console.log(i)
+}
+
+////////////////////////////////
+// Get Even
+////////////////////////////////
+//Create a for loop that prints the even numbers in 0 through 200. Create a number variable equal to 0; make the variable less than or equal to 200. Write a function that continuously adds the number 2 to the the variabble within the set parameters. Hint: think about the increment expression.
+//let x = 2
+//for (let i = 0; i <= 200;) {
+//  console.log(x + i++)
+//}
+for (let i = 0; i <= 200; i+=2) {
+  console.log(i)
+}
+//citation: https://linuxhint.com/increment-by-2-in-for-loop-in-javascript/#:~:text=To%20increment%20by%202%20in%20a%20for%20loop%2C%20use%20the,final%20or%20incremental%20expression%20statement.
+
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+//Create a javascript application that logs all the numbers from 1 through 100. If a number is divisible by 3 log "Fizz" instead of the number. If a number is divisible by 5 log "Buzz" instead of the number. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
