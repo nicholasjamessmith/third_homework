c619ebc2d9667dcf9630c7af5e3c4089643bb75e
nicholasjamessmith <njsmith6@gmail.com>
2023-05-05 15:41:46 -0400

    ninja turtles, methods, where's waldo answered

--- answers.js
+++ answers.js
@@ -41,7 +41,7 @@ for (let i = 0; i <= 200; i += 2) {
 //  if (i % 3 == 0) {
 //    console.log("Buzz")
 //  }
-//  if (i % 3 == 0); (i % 5 == 0) {
+//  if (i % 15 == 0) {
 //    console.log("FizzBuzz")
 //  }
 //}
@@ -79,56 +79,105 @@ console.log(wolfy)
 // Yell at the Ninja Turtles
 ////////////////////////////////
 //Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
-const donatello = []
-const leonardo = []
-const raphael = []
-const michaelangelo = []
-
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
 //Use a for of loop (not a typo - try it out! Try a for of loop) to call toUpperCase() on each of them and print out the result.
+for (each in ninjaTurtles) {
+  console.log(ninjaTurtles[each].toUpperCase())
+}
 
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
 //Here is a list of favMovies:
-//const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 //Console log: the index of Titanic
+console.log(favMovies[8])
 
 //Do the following and console.log the final results (I have included some thought questions, you don't have to write out an answer for those marked as such):
 //use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
+console.log(favMovies)
+favMovies.sort
+console.log(favMovies) //the .sort method logged the list of movies in a verticle list. The change appears to be permanent so long as the .sort method is applied either above or below the .sort method code.
+console.log(favMovies)
 //Use the method pop
+favMovies.pop();
+console.log(favMovies)
 //push "Guardians of the Galaxy"
+favMovies.push("Guardians of the Galaxy")
+console.log(favMovies)
 //Reverse the array
+favMovies.reverse();
+console.log(favMovies)
 //Use the shift method
+favMovies.shift();
+console.log(favMovies);
+favMovies.shift();
+console.log(favMovies)
 //unshift - what does it return?
+favMovies.unshift()
+console.log(favMovies)
+//returns the same list as last log, but when I tried favMovies.unshift("Movie") it added "Movie" to the top of the array.
 //splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
+//"Avatar" showed up outside of the array.
+//favMovies.splice("Django Unchained")
+//favMovies.push("Avatar")
+console.log(favMovies)
+//for (each in favMovies) {
+//  if each = "Django Unchained"
+//  console.log[each]("Django Unchained")
+//}
+for (item in favMovies) {
+  if (favMovies[item] === "Django Unchained") {
+    console.log(item)
+  }
+}
 //slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
-//store the value of your slice in a variable, console.log it - Thought question: what is going on here?
-//console.log your final results
-//use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
-//Use the method pop
-//push "Guardians of the Galaxy"
-//Reverse the array
-//Use the shift method
-//unshift - what does it return?
-//splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
-//slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+//Pseduocode:
+//determine the number of items in the array
+//console.log((favMovies).length)
+//find median
+//console.log(favMovies.median)
+//write a statement that returns the index items greater than or equal to the median index number
+//slice the index items greater than or equal to the median index number
+//let i = favMovies.length;
+//console.log(i)
+//const median = arr => {
+//  const mid = Math.floor(arr.length / 2),
+//    nums = [...arr].sort((a, b) => a - b);
+//  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+//};
+//citation: https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-88.php
+//console.log(median(favMovies))
+//let answer = (indexOf(favMovies) >= indexOf("Titanic"));
+//console.log(favMovies.length)
 //store the value of your slice in a variable, console.log it - Thought question: what is going on here?
 //console.log your final results
 
 //After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+console.log(favMovies.indexOf("Titanic"))
+console.log(favMovies.indexOf("Fast and Furious"))
+//indexOf "Fast and Furious" returned a value of -1, perhaps meant to indicate that it is not in the array.
 
-//Thought question: that we declared the variable favMovies with const, and yet, we were allowed to change the array. Weird? Should we have used let?
+// Thought question: that we declared the variable favMovies with const, and yet, we were allowed to change the array.Weird?Should we have used let?
+//The value of a const variable cannot be changed, but if the constant is an object or array its properties can be updated/removed 
 
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
 //With the following multi-dimensional array
-//const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-//                    ["Lucinda", "Jacc", "Neff", "Snoop"],
-//                    ["Petunia", ["Baked Goods", "Waldo"]]];
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+["Lucinda", "Jacc", "Neff", "Snoop"],
+["Petunia", ["Baked Goods", "Waldo"]]];
 //Remove Eggbert (hint look at the slice/splice method(s))
+console.log(whereIsWaldo);
+whereIsWaldo.splice(1, 1);
+console.log(whereIsWaldo)
 //Change "Neff" to "No One"
-//Access and console.log "Waldo"
+//whereIsWaldo.splice(2, 2, "No One")
+//console.log(whereIsWaldo)
+//console.log(whereIsWaldo)
+////Access and console.log "Waldo"
+//console.log(whereIsWaldo.indexOf("Waldo"))
 
 ////////////////////////////////
 //  Excited Kitten
